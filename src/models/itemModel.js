67fcf9d79002854e2d3410dee24a07f9bc94461e
tanermaddocks <maddockstaner@gmail.com>
2025-04-23 20:02:14 +1000

    feat: added first model and tested server, runs well

---
+++ src/models/itemModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    quantityInStock: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    category: {
+        type: String,
+        default: "General"
+    }
+}, {timestamps: true});
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item
