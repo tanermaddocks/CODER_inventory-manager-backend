e36ab98459b7d016b823ac96562013686c214b33
tanermaddocks <maddockstaner@gmail.com>
2025-04-23 20:49:33 +1000

    test: added one test and update package.json scripts

--- tests/items.test.js
+++ tests/items.test.js
@@ -5,14 +5,17 @@
 //      expect(something).someMatchers(something);
 // })
 
+// Importing a testing helper function from supertest
+const request = require('supertest');
+
+// Importing the code that we want to test
 const app = require("../src/index");
 
 describe('GET /health', () => {
-  it('should return status OK', async () => {
-    const respone = await request(app)
-      .get('/health');
-    expect(app.response.statusCode).toBe(200);
-    expect(app.response.body).toEqual({ status: OK })
-
-  })
-})
+    it('should return status OK', async() => {
+        const response = await request(app)
+        .get('/health');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({ status: "OK" });
+    });
+});
